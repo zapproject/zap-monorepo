2e9fe7991f951ec88c0d3c8a35a5f7ab8baead08
kb0911sm <k.bui1109@gmail.com>
2018-07-08 22:06:14 -0600

    - Base contract setup for different artifacts dir

--- packages/BaseContract/src/index.ts
+++ packages/BaseContract/src/index.ts
@@ -1,20 +1,28 @@
 const assert = require("assert");
 const Web3 = require("web3");
 const Artifacts = require("artifacts");
+import {contractsLoader} from '@zap/utils'
 import {baseContractType} from "./types";
 
 class Base {
-    constructor({contract,networkId,provider}:baseContractType) {
+    constructor({artifactsDir,artifactName,networkId,provider}:baseContractType) {
+      let artifact;
         try {
-            let artifact = Artifacts[contract];
-            this.provider = provider ||
-                new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545');
-            //network id default to mainnet
-            this.networkId = networkId || 1;
-            this.DEFAULT_GAS = 400000;
-            this.web3 = new Web3(this.provider);
-            this.contract = new this.web3.eth.Contract(artifact.abi,artifact.networks[this.networkId].address)
-            console.log(this.contract)
+          if(!artifactsDir){
+            artifact = Artifacts[artifactName];
+          }
+          else{
+            let artifacts = contractLoader(artifactsDir)
+            artifact = artifacts[artifactName];
+          }
+          this.provider = provider ||
+              new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545');
+          //network id default to mainnet
+          this.networkId = networkId || 1;
+          this.DEFAULT_GAS = 400000;
+          this.web3 = new Web3(this.provider);
+          this.contract = new this.web3.eth.Contract(artifact.abi,artifact.networks[this.networkId].address)
+          console.log(this.contract)
 
         } catch (err) {
             throw err;
@@ -23,4 +31,3 @@ class Base {
 }
 
 module.exports = Base;
-
