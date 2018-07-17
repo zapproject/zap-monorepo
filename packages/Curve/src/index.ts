487961df68faab6a79875c4ea0c593d2d6a293dd
Michael Man <17michaelm@gmail.com>
2018-07-17 18:23:28 -0400

    Added Curve tests and a validation function

--- packages/Curve/src/index.ts
+++ packages/Curve/src/index.ts
@@ -1,6 +1,7 @@
 import {CurveType, CurveTerm} from "./types";
 const {toHex}  = require("web3-utils");
-import {BigNumber} from 'bignumber.js'
+const BigNumber = require('big-number');
+
 export class Curve {
 
     constants : Array<number>;
@@ -14,13 +15,47 @@ export class Curve {
         this.parts = parts;
         this.dividers = dividers;
         this.pieces = Array();
+        this.checkValidity();
         this.structurize();
     }
+
+    /* 
+     * Checks that the piecewise curve encoding is valid. Throws an error if not
+     */
+     checkValidity():void {
+        if(this.constants.length % 3 != 0) throw new Error("Invalid number of constants arguments");
+        for(var i=0; i<this.constants.length; i++){
+            let c = this.constants[i];
+            if(!Number.isInteger(+c)) throw new Error("Constants must be integers");
+            if(i % 3 == 1 && c < 0) throw new Error("Cannot have a negative power");
+            if(i % 3 == 2 && (c < 0 || c > 1)) throw new Error("Unknown function encoding");
+        }
+        if(this.parts.length / 2 != this.dividers.length) throw new Error("A range must be defined for each constants triplet");
+
+        var prev = 0;
+        for(var i=0; i<this.parts.length; i++){
+            var p = this.parts[i];
+            if(i%2 == 0){
+                if(i!= 0 && p != prev) throw new Error("Parts ranges must be continuous");
+            } else {
+                if(p <= prev) throw new Error("Parts ranges must have a positive size");
+            }
+            if(p < 0 || !Number.isInteger(p)) throw new Error("Parts must be unsigned integers");
+            prev = p;
+        }
+
+        prev = this.dividers[0];
+        for(var i=0;i<this.dividers.length; i++){
+            if(this.dividers[i] <= prev && i!=0) throw new Error("Dividers must be ascending integers");
+            if(this.dividers[i] > this.constants.length/3) throw new Error("Dividers refer to a non-existent piecewise term");    
+        }
+
+}
 
     /**
      * Turn constants, parts, dividers into curve's coef, power, fn, pieces
      */
-    structurize() :void{
+     structurize() :void{
         let pStart = 0;
 
         for (let i = 0; i < this.dividers.length; i++) {
@@ -49,7 +84,7 @@ export class Curve {
      * @param {number} total bound dots
      * @returns {number}
      */
-    getPrice(total:number):number {
+     getPrice(total:number):number {
         if (total < 0) {
             return 0;
         }
@@ -74,12 +109,12 @@ export class Curve {
      * @returns {number}
      * @private
      */
-    _calculateTerm(term:CurveTerm, x:number) :number{
+     _calculateTerm(term:CurveTerm, x:number) :number{
         let val = 1;
 
-        if (term.fn === 0) {
+        if (term.fn === 0) { // absolute value
             if (x < 0) x = -x;
-        }  else if (term.fn === 1) {
+        }  else if (term.fn === 1) { // log
             if (x < 0) {
                 x = 0;
             } else  {
@@ -98,6 +133,7 @@ export class Curve {
      * Convert this curve constants, parts, dividers into Array of Bignumbers
      * @returns {Array<Array<BigNumber>>}
      */
+     /*
     convertToBNArrays():Array<Array<BigNumber>> {
         let convertedConstants = this.constants.map((item: number) => {
             return toHex(item);
@@ -110,7 +146,7 @@ export class Curve {
         });
         return [convertedConstants, convertedParts, convertedDividers];
 
-    }
+    }*/
 
     /**
      * Calculate total of terms
@@ -119,7 +155,7 @@ export class Curve {
      * @returns {number}
      * @private
      */
-    _calculatePolynomial(terms:any, x:number):number {
+     _calculatePolynomial(terms:any, x:number):number {
         let sum = 0;
 
         for (let i = 0; i < terms.length; i++ ) {
--- packages/Curve/test/curveTest.ts
+++ packages/Curve/test/curveTest.ts
@@ -1,1 +1,110 @@
 import {Curve} from "./../src";
+
+import {join} from "path";
+
+const expect = require('chai')
+.use(require('chai-as-promised'))
+.use(require('chai-bignumber'))
+.expect;
+
+describe('Zap Curve Test"', () => {
+
+	before(function (done) {
+    	// do nothing
+    	done();
+    });
+
+	it("1) Try to initialize a simple curve", async () => {
+		let constants = [2,2,0]; // 2x^2
+		let parts = [1,100];
+		let dividers = [1];
+		let curve = new Curve(constants, parts, dividers);
+	})
+
+	it("2) Perform a simple calculation", async () => {
+		let constants = [2,2,0]; // 2x^2
+		let parts = [1,100];
+		let dividers = [1];
+		let curve = new Curve(constants, parts, dividers);
+
+		var x = 10;
+		expect(curve.getPrice(x)).to.equal(2*x**2);
+	});
+
+	it("3) Perform calculations with a curve with multiple pieces", async () => {
+		let constants = [2,2,0,3,3,0]; // 2x^2, 3x^3
+		let parts = [1,10, 10, 50];
+		let dividers = [1, 2];
+		let curve = new Curve(constants, parts, dividers);
+
+		// test piece 1
+		var x = 10;
+		expect(curve.getPrice(x)).to.equal(2*x**2);
+		// test piece 2
+		x = 40;
+		expect(curve.getPrice(x)).to.equal(3*x**3);
+	});
+
+	it("4) Perform calculations with a curve with multiple parts to a piece", async () => {
+		let constants = [2,2,0,3,3,0]; // 2x^2+3x^3
+		let parts = [1,10];
+		let dividers = [2];
+		let curve = new Curve(constants, parts, dividers);
+		var x = 5;
+		expect(curve.getPrice(x)).to.equal(2*x**2 + 3*x**3);
+	});
+
+	it("5) Perform calculations with a curve with multiple parts to multiple pieces", async () => {
+		let constants = [2,2,0,3,3,0, 1,1,1, 1,1,0]; // 2x^2+3x^3 , lg(x) + x
+		let parts = [1,10, 10, 100];
+		let dividers = [2, 4];
+		let curve = new Curve(constants, parts, dividers);
+
+		var x = 5;
+		expect(curve.getPrice(x)).to.equal(2*x**2 + 3*x**3);
+		x = 40;
+		expect(curve.getPrice(x)).to.equal(Math.log2(x) + x);
+	});
+
+	it("6) Attempt to create curves with incorrect number of arguments", async () => {
+		let constants = [2,2,0,3,3,0,1]; // wrong number of constants
+		let parts = [1,10,50];
+		let dividers = [2];
+
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/Invalid number of constants arguments/);
+
+		constants = [2,2,0,3,3,0];
+		parts = [1,10, 10]; // wrong number of parts
+		dividers = [2];
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/A range must be defined for each constants triplet/);
+
+		constants = [2,2,0,3,3,0];
+		parts = [1,10];
+		dividers = [1,2];
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/A range must be defined for each constants triplet/);
+	});
+
+	it("7) Attempt to create curves with bad arguments", async () => {
+		let constants = [2,-1,0,3,3,0]; // negative power
+		let parts = [1,10];
+		let dividers = [2];
+
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/Cannot have a negative power/);
+
+		constants = [2,2,5]; // undefined function
+		parts = [1,10]; // wrong number of parts
+		dividers = [1];
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/Unknown function encoding/);
+
+		constants = [2,2,0,3,3,0]; 
+		parts = [1,10, 9, 20]; // overlapping ranges
+		dividers = [1, 2];
+		var bad = function(){ new Curve(constants, parts, dividers); };
+		expect(bad).to.throw(/Parts ranges must be continuous/);
+	});
+});
