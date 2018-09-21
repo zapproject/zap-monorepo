38192b24409e8b30e46762de32821a08d47cffdb
minciong <maxraphael_inciong@yahoo.com>
2018-09-21 15:41:03 -0400

    package.json edits, Bondage test edit

--- packages/Bondage/test/utils/setup_test.ts
+++ packages/Bondage/test/utils/setup_test.ts
@@ -17,10 +17,9 @@ export async function bootstrap(zapProvider: any, accounts: string[], deployedRe
     await deployedRegistry.contract.methods.initiateProvider(normalizedP.pubkey, normalizedP.title).send(defaultTx);
     const convertedCurve = zapProvider.curve.convertToBNArrays();
     const tokenOwner = await deployedToken.contract.methods.owner().call();
-    await deployedRegistry.contract.methods.initiateProviderCurve(normalizedP.endpoint,convertedCurve,null).send(defaultTx);
+    await deployedRegistry.contract.methods.initiateProviderCurve(normalizedP.endpoint,convertedCurve,"0x0000000000000000000000000000000000000000").send(defaultTx);
     //Endpoint with broker address
     await deployedRegistry.contract.methods.initiateProviderCurve(utf8ToHex(Utils.Constants.EndpointBroker),convertedCurve,accounts[5]).send(defaultTx);
-
     for (const account of accounts) {
         await deployedToken.contract.methods.allocate(account, Utils.toZapBase(100000000000000)).send({from: tokenOwner, gas: Utils.Constants.DEFAULT_GAS});
     }
