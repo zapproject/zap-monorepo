cfcecf5657da235a8813753a4249b9369cabe76e
minciong <maxraphael_inciong@yahoo.com>
2018-09-24 12:29:53 -0400

    Provider Subscriber Zaptoken still need work

--- packages/Subscriber/test/utils/setup_test.ts
+++ packages/Subscriber/test/utils/setup_test.ts
@@ -14,16 +14,16 @@ import {Utils} from "@zapjs/utils";
 export async function bootstrap(zapProvider:any,accounts:Array<string>,zapRegistry:any, zapToken:any){
     let normalizedP = Utils.normalizeProvider(zapProvider);
     let defaultTx = {from:accounts[0], gas:Utils.Constants.DEFAULT_GAS};
-    await zapRegistry.contract.methods.initiateProvider(normalizedP.pubkey,normalizedP.title).send(defaultTx);
+    await zapRegistry.contract.methods.initiateProvider(normalizedP.pubkey.toString(),normalizedP.title).send(defaultTx);
     let convertedCurve = zapProvider.curve.convertToBNArrays();
     let tokenOwner = await zapToken.contract.methods.owner().call();
-    await zapRegistry.initiateProviderCurve({endpoint:zapProvider.endpoint,term:zapProvider.curve.values, broker: "0x0",from:accounts[0]});
+    await zapRegistry.initiateProviderCurve({endpoint:zapProvider.endpoint,term:zapProvider.curve.valuesToString(convertedCurve), broker: "0x0000000000000000000000000000000000000000",from:accounts[0]});
     let providerCurve = await zapRegistry.getProviderCurve(accounts[0],zapProvider.endpoint);
     let endpointBroker = await zapRegistry.getEndpointBroker(accounts[0],normalizedP.endpoint);
     console.log("provider curve", providerCurve);
     console.log("endpoint broker: ", endpointBroker);
     for(let account of accounts) {
-        await zapToken.contract.methods.allocate(account, Utils.toZapBase(1000)).send({from: tokenOwner,gas:Utils.Constants.DEFAULT_GAS});
+        await zapToken.contract.methods.allocate(account, Utils.toZapBase(1000).toString()).send({from: tokenOwner,gas:Utils.Constants.DEFAULT_GAS});
     }
     return "done";
 }
