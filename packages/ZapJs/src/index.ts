6ab4b3ee3c66292a96e0dc67167d375d6fc61390
Pavel <pavel.b@smartum.pro>
2019-02-14 13:50:15 +0300

    fix zapjs package export

--- packages/ZapJs/src/index.ts
+++ packages/ZapJs/src/index.ts
@@ -1,14 +1,14 @@
 
 
-import {Artifacts} from "@zapjs/artifacts"
-import {ZapRegistry} from "@zapjs/registry";
-import {ZapArbiter} from "@zapjs/arbiter";
-import {ZapDispatch} from "@zapjs/dispatch";
-import {ZapBondage} from "@zapjs/bondage";
-import {Curve} from "@zapjs/curve";
-import {ZapToken} from "@zapjs/zaptoken";
-import {ZapProvider} from "@zapjs/provider";
-import {ZapSubscriber} from "@zapjs/subscriber";
+import { ZapArbiter } from "@zapjs/arbiter";
+import { Artifacts } from "@zapjs/artifacts";
+import { ZapBondage } from "@zapjs/bondage";
+import { Curve } from "@zapjs/curve";
+import { ZapDispatch } from "@zapjs/dispatch";
+import { ZapProvider } from "@zapjs/provider";
+import { ZapRegistry } from "@zapjs/registry";
+import { ZapSubscriber } from "@zapjs/subscriber";
+import * as Types from '@zapjs/types';
+import { ZapToken } from "@zapjs/zaptoken";
 
-export const zapjs ={Artifacts, ZapRegistry,ZapArbiter,ZapDispatch,ZapBondage,Curve,ZapToken,ZapProvider,ZapSubscriber}
-
+export { Artifacts, ZapRegistry, ZapArbiter, ZapDispatch, ZapBondage, Curve, ZapToken, ZapProvider, ZapSubscriber, Types };
